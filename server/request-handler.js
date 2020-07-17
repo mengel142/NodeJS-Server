13d14bca75786f36cebed29bbd06ace834480f0e
Matthew Engel <matthew.engel88@gmail.com>
2020-07-17 19:08:15 -0400

    initial set up and get request complete

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,5 +1,8 @@
-/*************************************************************
+var urlParser = require('url');
+const { parse } = require('querystring');
 
+
+/*************************************************************
 You should implement your request handler function in this file.
 
 requestHandler is already getting passed to http.createServer()
@@ -11,6 +14,17 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
+
+var messages = {
+  results: []
+};
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -39,11 +53,11 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  headers['Content-Type'] = 'applicaton/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  // response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +66,27 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  // response.end('Hello, World!');
+  if (request.url === '/classes/messages') {
+    if (request.method === 'GET') {
+      response.writeHead(200, headers).end(JSON.stringify(messages));
+    }
+    if (request.method === 'POST') {
+      let body = '';
+      request.on('data', chunk => {
+        console.log('chunk: recieved', chunk.toString());
+        body += chunk.toString();
+
+      });
+      request.on('end', () => {
+        let message = parse(body);
+        messages.results.push(message);
+      });
+      response.writeHead(201, headers).end(JSON.stringify(messages));
+    }
+  } else {
+    response.writeHead(404);
+  }
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -64,10 +98,6 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
 
+
+exports.requestHandler = requestHandler;
